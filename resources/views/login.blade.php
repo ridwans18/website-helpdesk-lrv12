b244edd37e06fc7259994e19620c220fe3c34fe1
ridwan satria <98225653+ridwans18@users.noreply.github.com>
2025-06-26 10:56:28 +0700

    update minor desain.

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,3 @@
-{{-- @if(session('error'))
-    <p class="text-red-600 text-center mt-4">{{ session('error') }}</p>
-@endif --}}
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,20 +19,20 @@
     <!-- Login Form -->
     <div class="flex-grow flex items-center justify-center pt-20">
         <div class="w-full max-w-md bg-white shadow-lg rounded-lg p-6">
-            <h3 class="text-2xl font-semibold text-center text-gray-700 mb-6">Masuk</h3>
+            <h3 class="text-2xl font-semibold text-center text-gray-700 mb-6">Login</h3>
 
-            <form method="POST" action="{{ route('login.proses') }}">
+            <form method="POST" action="{{ route('login') }}">
                 @csrf
 
                 <div class="mb-4">
-                    <label for="email" class="block text-sm font-medium text-gray-700">Alamat Email</label>
+                    <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
                     <input type="email" name="email" id="email" placeholder="name@example.com" required
                         class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
                 </div>
 
                 <div class="mb-4">
-                    <label for="password" class="block text-sm font-medium text-gray-700">Kata Sandi</label>
-                    <input type="password" name="password" id="password" placeholder="Password" required
+                    <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+                    <input type="password" name="password" id="password" placeholder="password" required
                         class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
                 </div>
 
@@ -51,8 +47,6 @@
                                 d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708" />
                         </svg>
                     </button>
-
-                    <a href="#" class="text-sm text-gray-500 hover:text-gray-700 font-bold">Lupa password?</a>
                 </div>
             </form>
         </div>
