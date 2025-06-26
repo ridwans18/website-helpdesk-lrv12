ea3a3d630b0ef81c05ed8aa2f10b287f330641c6
ridwan satria <98225653+ridwans18@users.noreply.github.com>
2025-06-26 10:36:27 +0700

    memberikan nilai dari elemen id dan name

--- resources/views/components/beranda-layout/rinciantambahan.blade.php
+++ resources/views/components/beranda-layout/rinciantambahan.blade.php
@@ -1,11 +1,13 @@
+@props(['showEditButton'])
+
 <!-- Extended Information -->
 <h4 class="text-lg font-semibold mb-2">
     Rincian
 </h4>
 <div class="mb-6">
     <textarea 
-        id="deskripsi" 
-        name="deskripsi"
+        id="rincian" 
+        name="rincian"
         rows="4" 
         class="w-full border border-gray-300 rounded px-3 py-2" 
         placeholder="Tambahan?"></textarea>
